320afec3df7de5aada37b2e0b921f7a61de8d54b
MichalskiA <maszjojo1@gmail.com>
2021-03-18 15:10:59 +0100

    Add ClientReservation model and init db context class

---
+++ Server-Module/Server/Database/Models/ClientReservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Database.Models
+{
+    public class ClientReservation
+    {
+        //Properties
+        public int ReservationID { get; set; }
+        public int RoomID { get; set; }
+        public int ClientID { get; set; }
+        //ReservationInfo Properties
+        public int HotelID { get; set; }
+        public int OfferID { get; set; }
+        public DateTime FromTime { get; set; }
+        public DateTime ToTime { get; set; }
+        public uint NumberOfChildren { get; set; }
+        public uint NumberOfAdults { get; set; }
+
+        
+    }
+}
--- Server-Module/Server/Database/ServerDBContext.cs
+++ Server-Module/Server/Database/ServerDBContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Server.Database
 {
-    public class ServerDBContext
+    public class ServerDBContext:DbContext
     {
+        public ServerDBContext(DbContextOptions<ServerDBContext> options) : base(options)
+        {
+        }
+        //FluentAPI    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
