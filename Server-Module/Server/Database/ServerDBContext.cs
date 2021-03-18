3556d6426b23e5a9d38cf966463ebd2961f7964d
MichalskiA <maszjojo1@gmail.com>
2021-03-18 16:06:03 +0100

    Add all DbSets (tables)

--- Server-Module/Server/Database/ServerDBContext.cs
+++ Server-Module/Server/Database/ServerDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Server.Database.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,18 @@ namespace Server.Database
         public ServerDBContext(DbContextOptions<ServerDBContext> options) : base(options)
         {
         }
+        //Tables
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ClientReservation> ClientReservations { get; set; }
+        public DbSet<ClientReview> ClientReviews { get; set; }
+        public DbSet<HotelInfo> HotelInfos { get; set; }
+        public DbSet<HotelPicture> HotelPictures { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<OfferHotelRoom> OfferHotelRooms { get; set; }
+        public DbSet<OfferPicture> OfferPictures { get; set; }
+
+
         //FluentAPI    
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
