af4b511f5dcf6511f5129adc6f43a0ae18e7954f
MichalskiA <maszjojo1@gmail.com>
2021-03-18 16:14:27 +0100

    Add Primary Keys for tables

--- Server-Module/Server/Database/ServerDBContext.cs
+++ Server-Module/Server/Database/ServerDBContext.cs
@@ -12,13 +12,15 @@ namespace Server.Database
         public ServerDBContext(DbContextOptions<ServerDBContext> options) : base(options)
         {
         }
-        //Tables
+        //Client Tables
         public DbSet<Client> Clients { get; set; }
         public DbSet<ClientReservation> ClientReservations { get; set; }
         public DbSet<ClientReview> ClientReviews { get; set; }
+        //Hotel Tables
         public DbSet<HotelInfo> HotelInfos { get; set; }
         public DbSet<HotelPicture> HotelPictures { get; set; }
         public DbSet<HotelRoom> HotelRooms { get; set; }
+        //Offer Tables
         public DbSet<Offer> Offers { get; set; }
         public DbSet<OfferHotelRoom> OfferHotelRooms { get; set; }
         public DbSet<OfferPicture> OfferPictures { get; set; }
@@ -27,7 +29,27 @@ namespace Server.Database
         //FluentAPI    
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            //Client PrimaryKeys
+            modelBuilder.Entity<Client>()
+                .HasKey(c => c.ClientID);
+            modelBuilder.Entity<ClientReservation>()
+               .HasKey(cr => cr.ReservationID);
+            modelBuilder.Entity<ClientReview>()
+               .HasKey(cr => cr.ReviewID);
+            //Hotel PrimaryKeys
+            modelBuilder.Entity<HotelInfo>()
+               .HasKey(hi => hi.HotelID);
+            modelBuilder.Entity<HotelPicture>()
+               .HasKey(hp => hp.PictureID);
+            modelBuilder.Entity<HotelRoom>()
+               .HasKey(hr => hr.RoomID);
+            //Offer PrimaryKeys
+            modelBuilder.Entity<Offer>()
+               .HasKey(o => o.OfferID);
+            modelBuilder.Entity<OfferHotelRoom>()
+               .HasKey(ohr => new { ohr.OfferID, ohr.RoomID });
+            modelBuilder.Entity<OfferPicture>()
+               .HasKey(op => op.PictureID);
         }
     }
 }
