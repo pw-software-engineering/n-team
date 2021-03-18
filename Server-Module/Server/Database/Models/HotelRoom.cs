aa744d315ba34be0f5b605c85d0ca413c75da4e4
MichalskiA <maszjojo1@gmail.com>
2021-03-18 15:31:39 +0100

    Add HotelRoom model

---
+++ Server-Module/Server/Database/Models/HotelRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Database.Models
+{
+    public class HotelRoom
+    {
+        //Properties
+        public int RoomID { get; set; }
+        public int HotelID { get; set; }
+        public string HotelRoomNumber { get; set; }
+    }
+}
