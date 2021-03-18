5e46101c3edf78101cc323823869feef4edcb92d
MichalskiA <maszjojo1@gmail.com>
2021-03-18 15:32:57 +0100

    Add OfferHotelRoom model

---
+++ Server-Module/Server/Database/Models/OfferHotelRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Database.Models
+{
+    public class OfferHotelRoom
+    {
+        //Properties
+        public int OfferID { get; set; }
+        public int RoomID { get; set; }
+    }
+}
