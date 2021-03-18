1e3ddfbce24c6dc12cd2f59f17c5517d9885464a
MichalskiA <maszjojo1@gmail.com>
2021-03-18 15:55:01 +0100

    Add ClientReview model

---
+++ Server-Module/Server/Database/Models/ClientReview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Database.Models
+{
+    public class ClientReview
+    {
+        //Properties
+        public int ReviewID { get; set; }
+        public int ClientID { get; set; }
+        //ReviewInfo Properties
+        public int OfferID { get; set; }
+        public string Content { get; set; }
+        public uint Rating { get; set; }
+        public DateTime ReviewData { get; set; }
+    }
+}
