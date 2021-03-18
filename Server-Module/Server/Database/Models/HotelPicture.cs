8e8ebf74fd95c198c3880ae398735509f9e7212c
MichalskiA <maszjojo1@gmail.com>
2021-03-18 15:59:28 +0100

    Add HotelPicture model

---
+++ Server-Module/Server/Database/Models/HotelPicture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Database.Models
+{
+    public class HotelPicture
+    {
+        //Properties
+        public int HoteID { get; set; }
+        public int PictureID { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
