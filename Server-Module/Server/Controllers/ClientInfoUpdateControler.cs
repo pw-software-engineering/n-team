3d4fc61eacb3fbe230b3ffadc53db065a9233ac7
Ignacy Sujecki <61029787+cangi75@users.noreply.github.com>
2021-03-23 18:44:44 +0100

    2 classes suporting client added

---
+++ Server-Module/Server/Controllers/ClientInfoUpdateControler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ClientInfoUpdateControler : ControllerBase
+    {
+        private readonly ILogger<ClientInfoUpdateControler> _logger;
+
+        public ClientInfoUpdateControler(ILogger<ClientInfoUpdateControler> logger)
+        {
+            _logger = logger;
+        }
+        [Route("/client")]
+        [HttpPost]
+        public string Post()
+        {
+            return "";
+        }
+    }
+}
---
+++ Server-Module/Server/Controllers/ClientLoginControler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    //[Route("[controller]")]
+    public class ClientLoginControler : ControllerBase
+    {
+        
+        private readonly ILogger<ClientLoginControler> _logger;
+
+        public ClientLoginControler(ILogger<ClientLoginControler> logger)
+        {
+            _logger = logger;
+        }
+        [Route("/client")]
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
