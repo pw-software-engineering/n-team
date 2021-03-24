e9048fb1430551e0ea3c69e0622e9c9723c35c43
Mateusz Tabor <79534883+mtabor99-pw@users.noreply.github.com>
2021-03-24 15:05:44 +0100

    Fix: Add hotel offers model reference to controller

--- Client-Module/Client-Module/Controllers/SearchController.cs
+++ Client-Module/Client-Module/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Client_Module.Models;
 using Client_Module.ViewsTagID.Layout;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
