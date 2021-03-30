73be99d82a566530fa4aada3692701dfb43a86a1
daveedoo <73546387+daveedoo@users.noreply.github.com>
2021-03-30 15:38:34 +0200

    Rooms management page: modal, adding and deleting rooms

---
+++ Hotel-Module/Hotel/Models/Offer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Models
+{
+    //skeleton
+    public class Offer
+    {
+        public uint OfferID { get; set; }
+        public bool Active { get; set; }
+        public double CostPerChild { get; set; }
+        public double CostPerAdult { get; set; }
+        public uint MaxGuests { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PreviewPicture { get; set; }
+        public string[] Pictures { get; set; }
+        //public Room[] Rooms { get; set; }
+    }
+}
