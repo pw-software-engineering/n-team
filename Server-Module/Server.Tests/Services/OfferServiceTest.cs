6a9bad3a1b72e4492b6bb6b9dcea0c65b74c1e08
MichalskiA <maszjojo1@gmail.com>
2021-04-15 14:13:43 +0200

    Add class for OfferService Tests Add Moq package (v4.16.1)

---
+++ Server-Module/Server.Tests/Services/OfferServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Tests.Services
+{
+    public class OfferServiceTest
+    {
+        public OfferServiceTest()
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddEntityFrameworkSqlServer()
+            .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<ServerDbContext>();
+            builder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=ServerDbTests;Trusted_Connection=True;MultipleActiveResultSets=true")
+                    .UseInternalServiceProvider(serviceProvider);
+
+            _context = new ServerDbContext(builder.Options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            if (!_context.HotelInfos.Any())
+                Seed();
+
+            var config = new MapperConfiguration(opts =>
+            {
+                opts.AddProfile(new AutoMapperProfile());
+            });
+            _mapper = config.CreateMapper();
+
+            _dataAccess = new DataAccess(_mapper, _context);
+        }
+        private ServerDbContext _context;
+        private IMapper _mapper;
+        private DataAccess _dataAccess;
+    }
+}
