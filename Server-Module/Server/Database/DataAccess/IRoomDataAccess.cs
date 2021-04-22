3eb6dd78005f40950beb7d4f42444ec0c82da974
MichalskiA <maszjojo1@gmail.com>
2021-04-22 13:32:23 +0200

    Add IRoomDataAccess interface Add RoomView - view model for room

---
+++ Server-Module/Server/Database/DataAccess/IRoomDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using Server.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Database.DataAccess
+{
+    public interface IRoomDataAccess
+    {
+        List<RoomView> GetRooms(int hotelID, string hotelRoomNumber = null);
+        int AddRoom(int hotelID, string hotelRoomNumber);
+        void DeleteRoom(int hotelID, int roomID)
+    }
+}
