9c344ab9f41d46de12b4ae190df9d3f1ba60b42a
MichalskiA <maszjojo1@gmail.com>
2021-04-22 15:06:39 +0200

    Add RoomDataAccessTest class Prepare seed data for tests

---
+++ Server-Module/Server.Tests/Database/RoomDataAccessTest.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Server.AutoMapper;
+using Server.Database;
+using Server.Database.DataAccess;
+using Server.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Server.Tests.Database
+{
+    public class RoomDataAccessTest : IDisposable
+    {
+        #region TestsSetup
+        public RoomDataAccessTest()
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddEntityFrameworkSqlServer()
+            .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<ServerDbContext>();
+            builder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=ServerDbTests;Trusted_Connection=True;MultipleActiveResultSets=true")
+                    .UseInternalServiceProvider(serviceProvider);
+
+            _context = new ServerDbContext(builder.Options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            if (!_context.HotelRooms.Any())
+                Seed();
+
+            var config = new MapperConfiguration(opts =>
+            {
+                opts.AddProfile(new AutoMapperProfile());
+            });
+            _mapper = config.CreateMapper();
+
+            _dataAccess = new RoomDataAccess(_mapper, _context);
+        }
+        private void Seed()
+        {
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT HotelInfos ON");
+                _context.HotelInfos.AddRange(
+                    new HotelInfoDb { HotelID = 1, City = "TestCity1", Country = "TestCountry1", HotelDesc = "TestHotelDesc1", AccessToken = "TestAccessToken1", HotelName = "TestHotelName1", HotelPreviewPicture = "TestHotelPreviewPicture1" },
+                    new HotelInfoDb { HotelID = 2, City = "TestCity2", Country = "TestCountry2", HotelDesc = "TestHotelDesc2", AccessToken = "TestAccessToken2", HotelName = "TestHotelName2", HotelPreviewPicture = "TestHotelPreviewPicture2" },
+                    new HotelInfoDb { HotelID = 3, City = "TestCity3", Country = "TestCountry3", HotelDesc = "TestHotelDesc3", AccessToken = "TestAccessToken3", HotelName = "TestHotelName3", HotelPreviewPicture = "TestHotelPreviewPicture3" });
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT HotelInfos OFF;");
+
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT HotelRooms ON");
+                _context.HotelRooms.AddRange(
+                    new HotelRoomDb { RoomID = 1, HotelID = 2, HotelRoomNumber = "TestHotelRoomNumber1" },
+                    new HotelRoomDb { RoomID = 2, HotelID = 3, HotelRoomNumber = "TestHotelRoomNumber2" },
+                    new HotelRoomDb { RoomID = 3, HotelID = 3, HotelRoomNumber = "TestHotelRoomNumber3" });
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT HotelRooms OFF;");
+
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Offers ON");
+                _context.Offers.AddRange(
+                    new OfferDb { OfferID = 1, HotelID = 2, OfferTitle = "TestOfferTitle1", OfferPreviewPicture = "TestOfferPreviewPicture1", IsActive = true, IsDeleted = false, CostPerChild = 10, CostPerAdult = 11, MaxGuests = 1, Description = "TestDescription1" },
+                    new OfferDb { OfferID = 2, HotelID = 3, OfferTitle = "TestOfferTitle2", OfferPreviewPicture = "TestOfferPreviewPicture2", IsActive = true, IsDeleted = false, CostPerChild = 20, CostPerAdult = 22, MaxGuests = 2, Description = "TestDescription2" },
+                    new OfferDb { OfferID = 3, HotelID = 3, OfferTitle = "TestOfferTitle3", OfferPreviewPicture = "TestOfferPreviewPicture3", IsActive = false, IsDeleted = true, CostPerChild = 30, CostPerAdult = 33, MaxGuests = 3, Description = "TestDescription3" });
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Offers OFF;");
+
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT OfferHotelRooms ON");
+                _context.OfferHotelRooms.AddRange(
+                    new OfferHotelRoomDb { OfferID = 1, RoomID = 1 },
+                    new OfferHotelRoomDb { OfferID = 2, RoomID = 2 },
+                    new OfferHotelRoomDb { OfferID = 3, RoomID = 2 },
+                    new OfferHotelRoomDb { OfferID = 3, RoomID = 3 });
+                _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT OfferHotelRooms OFF;");
+
+                _context.SaveChanges();
+                transaction.Commit();
+            }
+        }
+        #endregion
+
+        private ServerDbContext _context;
+        private IMapper _mapper;
+        private RoomDataAccess _dataAccess;
+        
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+        }
+    }
+}
