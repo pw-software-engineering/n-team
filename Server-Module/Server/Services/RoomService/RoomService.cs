b681cb999c9cac7288e6eeaf3d6cc3dcbefdfaa6
MichalskiA <maszjojo1@gmail.com>
2021-04-25 14:11:17 +0200

    Add IRoomService methods Add RoomService

---
+++ Server-Module/Server/Services/RoomService/RoomService.cs
@@ -0,0 +1,27 @@
+﻿using Server.Services.Response;
+using Server.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services.RoomService
+{
+    public class RoomService : IRoomService
+    {
+        public IServiceResult AddRoom(int hotelID, string hotelRoomNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceResult DeleteRoom(int hotelID, int roomID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceResult GetHotelRooms(Paging paging, int hotelID, string hotelRoomNumber = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
