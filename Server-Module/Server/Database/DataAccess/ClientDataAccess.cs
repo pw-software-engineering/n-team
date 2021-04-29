6c61e9dce11c30c1406a7dd8e1f49d2f1ca7815f
MichalskiA <maszjojo1@gmail.com>
2021-04-29 16:46:18 +0200

    Remove transactions from DataAccess classes

--- Server-Module/Server/Database/DataAccess/ClientDataAccess.cs
+++ Server-Module/Server/Database/DataAccess/ClientDataAccess.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Server.Database.DataAccess
 {
-	public class ClientDataAccess : IClientDataAccess
-	{
+    public class ClientDataAccess : IClientDataAccess
+    {
         private readonly IMapper _mapper;
         private readonly ServerDbContext _dbContext;
         public ClientDataAccess(IMapper mapper, ServerDbContext dbContext)
@@ -15,17 +15,13 @@ namespace Server.Database.DataAccess
             _mapper = mapper;
             _dbContext = dbContext;
         }
-        
+
         public void UpdateClientInfo(int clientID, string username, string email)
         {
-            using (var transaction = _dbContext.Database.BeginTransaction())
-			{
-                var client = _dbContext.Clients.Find(clientID);
-                client.Username = username ?? client.Username;
-                client.Email = email ?? client.Email;
-                _dbContext.SaveChanges();
-                transaction.Commit();
-			}
+            var client = _dbContext.Clients.Find(clientID);
+            client.Username = username ?? client.Username;
+            client.Email = email ?? client.Email;
+            _dbContext.SaveChanges();
         }
     }
 }
