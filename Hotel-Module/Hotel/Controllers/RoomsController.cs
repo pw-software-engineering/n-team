ee0fc69a71a87f6992feacadedb962d6d80fac90
daveedoo <73546387+daveedoo@users.noreply.github.com>
2021-05-25 15:11:10 +0200

    Index controller and viewmodel

--- Hotel-Module/Hotel/Controllers/RoomsController.cs
+++ Hotel-Module/Hotel/Controllers/RoomsController.cs
@@ -1,17 +1,50 @@
 ﻿using Hotel.Models;
+using Hotel.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Hotel.Controllers
 {
     public class RoomsController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public RoomsController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient(nameof(DefaultHttpClient));
+        }
+
+
+        public async Task<IActionResult> Index([FromQuery] string roomNumber, [FromQuery] Paging paging)
+        {
+            NameValueCollection query = HttpUtility.ParseQueryString("");
+            if (!string.IsNullOrWhiteSpace(roomNumber))
+                query["roomNumber"] = roomNumber.ToString();
+            query["pageNumber"] = paging.PageNumber.ToString();
+            query["pageSize"] = paging.PageSize.ToString();
+
+            try
+            {
+                IEnumerable<Room> rooms = await _httpClient.GetFromJsonAsync<IEnumerable<Room>>("rooms?" + query.ToString());
+                RoomsIndexViewModel roomsVM = new RoomsIndexViewModel(rooms, paging, roomNumber);
+                return View(roomsVM);
+            }
+            catch (HttpRequestException e)
+            {
+                return StatusCode((int)(e.StatusCode ?? HttpStatusCode.InternalServerError));
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
         }
 
         public PartialViewResult OfferRowPartial(uint offerID)
