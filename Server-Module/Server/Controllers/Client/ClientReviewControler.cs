4527ff6c9585e829925493e8d51f96d5102ea371
Ignacy Sujecki <61029787+cangi75@users.noreply.github.com>
2021-05-29 18:55:32 +0200

    skelet

---
+++ Server-Module/Server/Controllers/Client/ClientReviewControler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Authentication.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers.Client
+{
+    [Authorize(AuthenticationSchemes = ClientTokenDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("/api-client")]
+    public class ClientReviewControler : ControllerBase
+    {
+        private int _clientID;
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var id = from claim in HttpContext.User.Claims
+                     where claim.Type == ClientTokenManagerOptions.ClientIdClaimName
+                     select int.Parse(claim.Value);
+            _clientID = id.First();
+            base.OnActionExecuting(context);
+        }
+        [HttpGet("reservations/{reservationID}/review")]
+        public IActionResult GetReview()
+        {
+            return null;
+        }
+
+        [HttpPut("reservations/{reservationID}/review")]
+        public IActionResult CreateReview()
+        {
+            return null;
+        }
+
+        [HttpDelete("reservations/{reservationID}/review")]
+        public IActionResult DeleteReview()
+        {
+            return null;
+        }
+    }
+}
---
+++ Server-Module/Server/Services/Client/ClientReviewService/ReviewService.cs
@@ -0,0 +1,26 @@
+﻿using Server.Services.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services.Client.ClientReviewService
+{
+    public class ReviewService : IReviewSerice
+    {
+        public IServiceResult DeleteReview(int reservationID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceResult GetReview(int reservationID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceResult PutReview(int reservationID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
